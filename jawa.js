5a8b68c27faba1ce9bbdbdd88a68ec52ec55d740
LiegeSphy <muhammadzakywijaya01@gmail.com>
2025-12-12 15:04:30 +0800

    Add files via upload

---
+++ jawa.js
@@ -0,0 +1,14 @@
+// const observer = new IntersectionObserver(entries => {
+//      entries.forEach(entry => {
+//         if(entry.isIntersecting){
+//             document.querySelectorAll(".tentanganim")[0].classList.add("fadeInLeft");
+//             document.querySelectorAll(".tentanganim")[1].classList.add("fadeintop");
+//             document.querySelectorAll(".tentanganim")[2].classList.add("fadeinright");
+//             // document.querySelectorAll(".fow")[0].classList.add("fadeindown");
+//         }
+//      })
+// })
+
+// observer.observe(document.querySelector(".hyperion"));
+// unused. Probably bc its for images. might use this later
+
